6cc1ed6bb7119bafe3cd0c38311ff3d667f61580
Alumno <alumno@localhost.localdomain>
2014-01-29 08:26:49 -0600

    first comit

---
+++ src/madn/miclase.java
@@ -0,0 +1,17 @@
+package madn;
+
+import java.util.Scanner;
+
+public class miclase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner mi_scanner = new Scanner(System.in);
+int mi_variable = mi_scanner.nextInt();
+if(mi_variable == 10)
+{
+	System.out.print("es un 10");
+}
+	}
+
+}
